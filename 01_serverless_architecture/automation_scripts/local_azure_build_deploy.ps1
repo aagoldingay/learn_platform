95e322eaa2c69518de711351e6ff9ad46ed2720c
Aaron Goldingay <Aaron.Goldingay@bjss.com>
2023-03-24 11:42:53 +0000

    tidied up local deploy script

--- 01_serverless_architecture/automation_scripts/local_azure_build_deploy.ps1
+++ 01_serverless_architecture/automation_scripts/local_azure_build_deploy.ps1
@@ -1,107 +1,158 @@
 param (
     [string]
-    $tenant_id,
+    $tenant_id = "",
 
     [bool]
     $deploy_azure = $true,
 
     [bool]
-    $deploy_gcp,
+    $deploy_gcp = $false,
 
     [bool]
-    $az_receiver
+    $az_receiver = $false,
+
+    [bool]
+    $skip_build = $false,
+
+    [bool]
+    $skip_test = $false,
+
+    [bool]
+    $skip_infra = $false,
+
+    [bool]
+    $tf_apply_only = $false,
+
+    [bool]
+    $skip_code_deploy = $false
 )
 
-Write-Host "# BUILDING serverless_app ..."
+function Evaluate_Output {
+    param(
+        [string]
+        $id,
 
-$build_output = $(dotnet build ../serverless_app "/p:DeployOnBuild=true /p:DeployTarget=Package;CreatePackageOnPublish=true")
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Warning -Message ($build_output -join "; ")
-    throw "# # BUILD FAILED"
-} else {
-    Write-Host "# # BUILD SUCCESSFUL"
-}
-
-Write-Host "# RUNNING TESTS IN serverless_app ..."
-
-$test_output = $(dotnet test ../serverless_app)
-
-if ($LASTEXITCODE -ne 0) {
-    Write-Warning -Message ($test_output -join "; ")
-    throw "# # TESTS FAILED"
-} else {
-    Write-Host "# # TESTS SUCCESSFUL"
-}
-
-if ($deploy_azure -and !$deploy_gcp) {
-    Write-Host "# PUBLISHING serverless_app/azure_host ..."
-    
-    $az_receiver_publish_output = $(dotnet publish ../serverless_app/azure_host_receiver -c Release -o ./az_serverless_receiver)
-    
-    if ($LASTEXITCODE -ne 0) {
-        Write-Warning -Message ($az_receiver_publish_output -join "; ")
-        throw "# # PUBLISH FAILED"
-    } else {
-        Write-Host "# # PUBLISH SUCCESSFUL"
-    }
-    
-    Compress-Archive -LiteralPath az_serverless_receiver -DestinationPath az_serverless_receiver.zip -Force
-
-    $az_sender_publish_output = $(dotnet publish ../serverless_app/azure_host_sender -c Release -o ./az_serverless_sender)
+        [string]
+        $log
+    )
 
     if ($LASTEXITCODE -ne 0) {
-        Write-Warning -Message ($az_sender_publish_output -join "; ")
-        throw "# # PUBLISH FAILED"
-    } else {
-        Write-Host "# # PUBLISH SUCCESSFUL"
+        Write-Warning "# # $id FAILED"
+        Write-Error $log
+        exit
     }
-    
-    Compress-Archive -LiteralPath az_serverless_sender -DestinationPath az_serverless_sender.zip -Force
+    else {
+        Write-Host "# # $id SUCCESSFUL"
+    }
 }
 
-# if ($deploy_gcp -and !$deploy_azure) {
-#     Write-Host "# PUBLISHING serverless_app/gcp_host ..."
+if (!$skip_build) {
+    Write-Host "# BUILDING serverless_app ..."
     
-#     $publish_output = $(dotnet publish ../serverless_app/gcp_host --os linux -o ./serverless_app)
+    $build_output = $(dotnet build ../serverless_app "/p:DeployOnBuild=true /p:DeployTarget=Package;CreatePackageOnPublish=true")
+
+    Evaluate_Output -id "BUILD" -log ($build_output -join "; ")
+}
+
+if (!$skip_test) {
+    Write-Host "# RUNNING TESTS IN serverless_app ..."
     
-#     if ($LASTEXITCODE -ne 0) {
-#         Write-Warning -Message ($build_output -join "; ")
-#         throw "# # PUBLISH FAILED"
-#     } else {
-#         Write-Host "# # PUBLISH SUCCESSFUL"
-#     }
+    $test_output = $(dotnet test ../serverless_app)
+
+    Evaluate_Output -id "TESTS" -log ($test_output -join "; ")
+}
+
+if (!$skip_code_deploy) {
+    if ($deploy_azure -and !$deploy_gcp) {
+        Write-Host "# PUBLISHING serverless_app ..."
+        
+        $az_receiver_publish_output = $(dotnet publish ../serverless_app/azure_host_receiver -c Release -o ./az_serverless_receiver)
+
+        Evaluate_Output -id "PUBLISH" -log ($az_receiver_publish_output -join "; ")
+        
+        Compress-Archive -LiteralPath az_serverless_receiver -DestinationPath az_serverless_receiver.zip -Force
+
+        $az_sender_publish_output = $(dotnet publish ../serverless_app/azure_host_sender -c Release -o ./az_serverless_sender)
+
+        Evaluate_Output -id "PUBLISH" -log ($az_sender_publish_output -join "; ")
+        
+        Compress-Archive -LiteralPath az_serverless_sender -DestinationPath az_serverless_sender.zip -Force
+    }
+
+    # if ($deploy_gcp -and !$deploy_azure) {
+    #     Write-Host "# PUBLISHING serverless_app/gcp_host ..."
+        
+    #     $publish_output = $(dotnet publish ../serverless_app/gcp_host --os linux -o ./serverless_app)
+        
+    #     Evaluate_Output
+        
+    #     Compress-Archive -LiteralPath serverless_app -DestinationPath gcp_serverless_app.zip
+    # }
+}
+
+if (!$skip_infra) {
+    $az_login_output = az login -t $tenant_id
+
+    Evaluate_Output -id "AZURE LOGIN" -log ($az_login_output -join "; ")
+
+    cd ../terraform
+    Write-Host "# INITIALISING TERRAFORM"
+
+    $tf_output = terraform init
+
+    Evaluate_Output -id "TERRAFORM INIT" -log ($tf_output -join "; ")
     
-#     Compress-Archive -LiteralPath serverless_app -DestinationPath gcp_serverless_app.zip
-# }
+    Write-Host "# VALIDATING TERRAFORM CONFIGURATION ..."
 
-# Write-Host "# DEPLOYING INFRASTRUCTURE ..."
+    $tf_output = terraform validate
 
-# if ($deploy_azure -and !$deploy_gcp) {
-#     az login -t $tenant_id
-#     cd ../terraform
-#     terraform plan -var-file az_only.tfvars
+    Evaluate_Output -id "TERRAFORM VALIDATE" -log ($tf_output -join "; ")
 
-# } elseif ($deploy_gcp -and !$deploy_azure) {
+    Write-Host "# DEPLOYING INFRASTRUCTURE ..."
+    
+    if ($deploy_azure -and !$deploy_gcp) {
+        $var_file = "az_only.tfvars"
+        
+    
+        # } elseif ($deploy_gcp -and !$deploy_azure) {
+    
+        #     Write-Host "# # GCP ONLY ..."
+    
+        # } else {
+    
+        #     Write-Host "# # AZ AND GCP"
+        #     az login -t $tenant_id
+    
+        #     if ($az_receiver) {
+    
+        #         # deploy where az hosts receiver
+    
+        #     } else {
+    
+        #         # deploy where gcp hosts receiver
+    
+        #     }
+    }
 
-#     Write-Host "# # GCP ONLY ..."
+    if (!$tf_apply_only) {
+        $tf_output = terraform plan -var-file=secrets.tfvars -var-file=$var_file
 
-# } else {
+        Evaluate_Output -id "TERRAFORM PLAN" -log ($tf_output -join "; ")
+    }
 
-#     Write-Host "# # AZ AND GCP"
-#     az login -t $tenant_id
+    $tf_output = terraform apply -var-file=secrets.tfvars -var-file=$var_file -auto-approve
 
-#     if ($az_receiver) {
+    Evaluate_Output -id "TERRAFORM APPLY" -log ($tf_output -join "; ")
+}
 
-#         # deploy where az hosts receiver
+if (!$skip_code_deploy) {
+    if ($deploy_azure -and !$deploy_gcp) {
+        $receiver_deploy_output = az functionapp deployment source config-zip -g 01_serverless -n 01-serverless-receiver --src az_serverless_receiver.zip
 
-#     } else {
+        Evaluate_Output -id "RECEIVER DEPLOY" -log ($receiver_deploy_output -join "; ")
+    
+        $sender_deploy_output = az functionapp deployment source config-zip -g 01_serverless -n 01-serverless-sender --src az_serverless_sender.zip
 
-#         # deploy where gcp hosts receiver
-
-#     }
-# }
-
-az functionapp deployment source config-zip -g 01_serverless -n 01-serverless-receiver --src az_serverless_receiver.zip
-
-az functionapp deployment source config-zip -g 01_serverless -n 01-serverless-sender --src az_serverless_sender.zip
+        Evaluate_Output -id "SENDER DEPLOY" -log ($sender_deploy_output -join "; ")
+    }
+}
